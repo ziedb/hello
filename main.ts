c4bae84622987a44d61ff297b2329793d344d835
Zied BOUZIRI <31895427+ziedb@users.noreply.github.com>
2024-11-21 10:11:45 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+function getTest(counter) {
+    switch (counter) {
+        case 1: return new Test1();
+    }
+}
+
+function display(data: int16[][]) {
+    /* Convert data in the good format : 1 is turn on, 0 is turn off */
+    basic.showLeds("")
+}
+const test = getTest(counter)
+const myData = myFunction(test.getInput());
+display(myData);
+
+const test = getTest(counter)
+checkTest(test, myFunction(test.getInput()))
